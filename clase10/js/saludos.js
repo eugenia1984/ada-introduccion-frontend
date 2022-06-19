c08b968b0fc5430a2f352eacbd9ba37c6d667a03
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-06-19 20:32:38 -0300

    agrego lso ejercicios de la clase 7

--- clase10/js/saludos.js
+++ clase10/js/saludos.js
@@ -6,15 +6,13 @@ let lastName = (prompt('Ingrese su apellido: ')).toUpperCase();
 // show a welcome massage with alert using the data the user provided
 let regex = /[0-9]+/; // regex that defines that can have one or more numbers 
 // check we do not have empty data
-if (firstName == '' || firstName == '') {
+if (firstName == '' || firstName == null  || firstName == '' ||firstName == null) {
   alert('Debe ingresar los datos solicitados!');
+}
+if ( firstName == null ||firstName == null) {
+  alert('No debe hacer click en cancelar!');
 } 
-// check the user do not click in cancel button
-if (firstName == null || firstName == null ) {
-  alert('No debe hacer click en cancelar');
-} 
-// check the user do not insert numbers
-if ( !regex.test(firstName) && !regex.test(lastName) ) {
+if ( !regex.test(firstName) && !regex.test(lastName) ) { // check the user do not insert numbers
   alert(`Hola ${firstName} ${lastName}, bienvenida a Ada`);
 } else {
   alert('Solo debe ingresar letras, no numeros');
