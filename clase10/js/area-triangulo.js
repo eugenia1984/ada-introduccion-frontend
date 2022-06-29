babcbd73ab3ceee8a9230a89c8fb53c8f605f83b
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-06-29 20:53:40 -0300

    redondeo el resultado que muestra el area del triangulo

--- clase10/js/area-triangulo.js
+++ clase10/js/area-triangulo.js
@@ -3,6 +3,6 @@
 let base = parseInt(prompt(`Ingrese la BASE del triangulo (en numeros enteros): `));
 // ask with prompt to introduce height and store the data in a variable
 let height = parseInt(prompt(`Ingrese la ALTURA del triangulo (en numeros enteros): `));
-let area =(base * height) / 2; // para calcular el area
+let area = Math.round( ((base * height) / 2) * 100) / 100; // para calcular el area con 2 decimales
 // Show the  area of a triangle with an alert
 alert(`El área del triangulo es: ${area}.`);
