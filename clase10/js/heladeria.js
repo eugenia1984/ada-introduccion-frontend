126412718fed98a900877cde6002bb47e0e2d023
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-06-29 18:25:46 -0300

    agrego el segundo ejercicio de la clase 10

---
+++ clase10/js/heladeria.js
@@ -0,0 +1,9 @@
+//prompt alert
+// ask with prompt to introduce the first ice-cream taste and store the data in a variable
+let firstTaste = (prompt('Ingrese su 1er gusto: ')).toUpperCase();
+// ask with prompt to introduce the second ice-cream taste and store the data in a variable
+let secondTaste = (prompt('Ingrese su 2do gusto: ')).toUpperCase();
+// ask with prompt to introduce the third ice-cream taste and store the data in a variable
+let thirdTaste = (prompt('Ingrese su 3er gusto: ')).toUpperCase();
+
+alert(`Aquí tiene su helado de ${firstTaste}, ${secondTaste} y ${thirdTaste}`)
--- clase10/js/saludos.js
+++ clase10/js/saludos.js
@@ -5,13 +5,16 @@ let firstName = (prompt('Ingrese su nombre: ')).toUpperCase();
 let lastName = (prompt('Ingrese su apellido: ')).toUpperCase();
 // show a welcome massage with alert using the data the user provided
 let regex = /[0-9]+/; // regex that defines that can have one or more numbers 
+
 // check we do not have empty data
 if (firstName == '' || firstName == null  || firstName == '' ||firstName == null) {
   alert('Debe ingresar los datos solicitados!');
 }
-if ( firstName == null ||firstName == null) {
+
+if ( firstName == null || firstName == null) {
   alert('No debe hacer click en cancelar!');
 } 
+
 if ( !regex.test(firstName) && !regex.test(lastName) ) { // check the user do not insert numbers
   alert(`Hola ${firstName} ${lastName}, bienvenida a Ada`);
 } else {
