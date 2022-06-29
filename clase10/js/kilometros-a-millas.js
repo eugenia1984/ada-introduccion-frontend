bfc1ce505906c44ff19ce9f1807cf272cee7ae21
Eugenia <costamariaeugenia1@gmail.com>
2022-06-29 20:36:55 -0300

    completo en kilometros-a-millas.js

---
+++ clase10/js/kilometros-a-millas.js
@@ -0,0 +1,6 @@
+//prompt alert
+// ask with prompt to introduce kilometers and store the data in a variable
+let kilometerss = parseInt(prompt(`Ingrese los KILOMETROS para calcular su equivalente en MILLAS\n (en numeros enteros): `));
+let miless = kilometers / 1.609344;
+// Show the equivalent of kilometers in miles with an alert
+alert(`Los ${kilometers} kilometros ingresados equivalen a ${miles} millas`);
