211250a78fe7ccdd66fdc42b9e6db7e83e733f71
Eugenia <costamariaeugenia1@gmail.com>
2022-06-29 20:13:01 -0300

    actualizo

---
+++ clase10/js/dias-a-segundos.js
@@ -0,0 +1,6 @@
+//prompt alert
+// ask with prompt to introduce days and store the data in a variable
+let days = parseInt(prompt(`Ingrese los DIAS para calcular su equivalente en SEGUNDOS\n (en numeros enteros): `));
+let seconds = days * 24 * 60;
+// Show the equivalent in minuts with an alert
+alert(`Los ${days} ingresados equivalen a ${seconds} segundos`);
