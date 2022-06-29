1774916350decd7f8ee296f967ec3b0a5ba6c9ce
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-06-29 20:43:13 -0300

    corrijo

--- clase10/js/kilometros-a-millas.js
+++ clase10/js/kilometros-a-millas.js
@@ -1,6 +1,6 @@
 //prompt alert
 // ask with prompt to introduce kilometers and store the data in a variable
-let kilometerss = parseInt(prompt(`Ingrese los KILOMETROS para calcular su equivalente en MILLAS\n (en numeros enteros): `));
-let miless = kilometers / 1.609344;
+let kilometers = parseInt(prompt(`Ingrese los KILOMETROS para calcular su equivalente en MILLAS\n (en numeros enteros): `));
+let miles = Math.round(kilometers / 1.609344 * 100) / 100; // para redondear a dos decimales
 // Show the equivalent of kilometers in miles with an alert
 alert(`Los ${kilometers} kilometros ingresados equivalen a ${miles} millas`);
