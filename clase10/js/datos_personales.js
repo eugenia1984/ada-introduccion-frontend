24c69accacf8fd1a2f9347ab0365e7d666f0b622
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-06-29 18:46:49 -0300

    agrego el tercer ejemplo de practica

---
+++ clase10/js/datos_personales.js
@@ -0,0 +1,14 @@
+// Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento, y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:" y liste todos los datos juntos.
+//prompt alert
+// ask with prompt to introduce the first name and store the data in a variable
+let firstName = (prompt('Ingrese su nombre: ')).toUpperCase();
+// ask with prompt to introduce the last name and store the data in a variable
+let lastName = (prompt('Ingrese su apellido: ')).toUpperCase();
+// ask with prompt to introduce the age and store the data in a variable
+let age = (prompt('Ingrese su edad: ')).toUpperCase();
+// ask with prompt to introduce the nationality and store the data in a variable
+let nationality = (prompt('Ingrese su nacionalidad: ')).toUpperCase();
+// ask with prompt to introduce the DNI and store the data in a variable
+let dni = (prompt('Ingrese su numero de DNI: ')).toUpperCase();
+// show all the data with an alert
+alert(`Nuevo usuario agregado.\n${firstName}\n${lastName}\n${age} años\nnacionalidad: ${nationality}\nDNI: ${dni}`)
