53d16504764fde4463faea65b2cb5a20a562c5c0
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-06-30 12:44:12 -0300

    agrego ejercicio

---
+++ clase10/js/c-a-f.js
@@ -0,0 +1,3 @@
+let celsiusTemperature = parseFloat(prompt('Ingrese la temperatura en grados Celcius : '));
+let fahrenheitTemperature = ( celsiusTemperature * 9/5) + 32;
+alert(`Los ${celsiusTemperature}ºC equivalen a  ${fahrenheitTemperature} F`);
